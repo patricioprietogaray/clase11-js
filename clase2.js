240cb6b58e52e60448ee24ef35192048e9e6ab26
patricioprietogaray <patricioprietogaray@gmail.com>
2022-06-25 18:32:28 -0300

    MASTER

--- clase2.js
+++ clase2.js
@@ -13,7 +13,7 @@ function funcionConArgumentosNoDevuelve(a, b) {
 
 function funcionSinArgumentosDevuelve() {
     console.log("Devuelve y solo muestra!");
-    return "retorno de informacion"
+    return "retorno de informacion";
 }
 
 function funcionConArgumentosDevuelve(a, b) {
@@ -69,3 +69,104 @@ console.log("3+7="+res);
 
 //////////////
 
+const result = suma();
+console.log(`El resultado es ${result}`);
+
+function suma(a=1, b=1) {
+    return a+b;
+}
+
+// ///////// hoistin o algo asi es cuando js permite
+// que la funcion se "mueva hacia arriba" para
+// permitir que se lea la funcion antes de ser
+// llamada, con la funcion expresada no se da,
+// por lo tanto tira error.
+
+// const result01= suma();
+
+// const suma = function () {
+//     return 10;
+// }
+
+// funcion anónima
+// solo funciona con un callback
+// function () {
+    // console.log("anonymous");
+// }
+// ej
+// function () {
+//     console.log("anonymous");
+// }
+
+const resta = (x, y) => {
+    return x-y;
+}
+
+const potenciacionCuadrado = x => {
+// como es un solo argumento no necesita parentesis
+// const a = (arg1) .... >  const a = arg1 ....
+    return x*x;
+}
+
+// otra forma de hacer lo mismo sería
+const potenciacionCubo = z => z*z*z;
+// sin {} y sin return en una misma linea
+
+console.log(`2 al cuadrado es: ${potenciacionCuadrado(2)}`);
+console.log(`2 al cubo es: ${potenciacionCubo(2)}`);
+
+// si no se pasan argumentos entonces muestra la 
+// funcion en pantalla
+
+////////////////
+
+let resultado05 = () => 3+4;
+console.log(resultado05());
+
+//ejemplos
+// funcion normal
+
+// function calcularAreaCirculo(radio) {
+//     return 3.14 * radio**2;
+//     // radio**2 es radio al cuadrado o radio*radio
+// }
+
+// arrow function
+// const calcularAreaCirculo = (radio) => {
+//     return 3.14 * radio**2;
+//     // radio**2 es radio al cuadrado o radio*radio
+// }
+
+
+// arrow function simplificada
+const calcularAreaCirculo = radio => 3.14*radio**2;
+
+const area = calcularAreaCirculo(5);
+console.log(area);
+
+
+/////SALUDO
+// funcion normal
+// const saludar = function(nombre, momentoDelDia) {
+//     const saludo = `Good ${momentoDelDia}, ${nombre}!`;
+//     return saludo;
+// }
+
+// no se puede prescindir del return ni {} porque hay mas de una linea
+const saludar = (nombre, momentoDelDia) => {
+    const saludo = `Good ${momentoDelDia}, ${nombre}!`;
+    return saludo;
+}
+
+const saludo = saludar('Patricio', 'Morning');
+const saludo2 = saludar('Seba', 'Night');
+const saludo3 = saludar('Nehuen', 'Afternoon');
+const saludo4 = saludar('Marimer', 'Morning');
+
+console.log(saludo);
+console.log(saludo2);
+console.log(saludo3);
+console.log(saludo4);
+
+
+///////ARREGLO o ARRAY
--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-const result = suma();
-console.log(`El resultado es ${result}`);
+var miPrimerArreglo=["hola", 1,  true, [ "a",   "b",   "c"]  ];
+// posiciones         [0]    [1]  [2]  [3][0]  [3][1]  [3][2]  
 
-function suma(a=1, b=1) {
-    return a+b;
-}
+console.log(miPrimerArreglo[3][2]);
+// arrary[posicionArrayContenedor][posicionArrayContenido];
 
-// ///////// hoistin o algo asi es cuando js permite
-// que la funcion se "mueva hacia arriba" para
-// permitir que se lea la funcion antes de ser
-// llamada, con la funcion expresada no se da,
-// por lo tanto tira error.
+var electrodomesticos=['lavarropa','heladera','microondas'];
+console.log(electrodomesticos);
+console.log(`el arreglo tiene ${electrodomesticos.length} elementos`);
+console.log(`elemento 0: ${electrodomesticos[0]}`);
+console.log(`elemento 1: ${electrodomesticos[1]}`);
+console.log(`elemento 2: ${electrodomesticos[2]}`);
+console.log(`**********************************`);
+console.log(`mutacion de arreglos o edicion`);
+console.log(`cambio el elemento 0: ${electrodomesticos[0]} como Cocina`);
+electrodomesticos[0]='Cocina';
+// en la posicion o elemento se le asigna un nuevo valor
+console.log(`elemento 0: ${electrodomesticos[0]}`);
+console.log(`elemento 1: ${electrodomesticos[1]}`);
+console.log(`elemento 2: ${electrodomesticos[2]}`);
 
-// const result01= suma();
+// Métod push (agregar) y pop eliminar (lifo last in first out)
 
-// const suma = function () {
-//     return 10;
-// }
+const estudiantes = ['Martin', 'Patricia', 'Fernando'];
+console.log(estudiantes);
+// agrego elemento al final
+estudiantes.push('Pepe');
+console.log(estudiantes);
+// elimino el ultimo elemento del array
+estudiantes.pop();
+console.log(estudiantes);
 
-// funcion anónima
-// solo funciona asignando una variable o constante
-// function () {
-    // console.log("anonymous");
-// }
-// ej
-function () {
-    console.log("anonymous");
-}
-// 30:00
+// filo?
+// unshift es push en el primer elemento (0)
+//  shift es pop en el primer elemento(0)
+console.log("uso de unshift (push elemento 0) y shift es pop del elem 0");
+console.log(estudiantes);
+estudiantes.unshift('primitivo');
+console.log(estudiantes);
+estudiantes.shift();
+console.log(estudiantes);
+
+///// SLICE para no modificar el array original se hace una copia y se 
+// trabaja con el duplicado.
+const estudiantesOriginal = ['Matias', 'Brian', 'Julieta', 'Gaston', 'Anita', 'Daiana'];
+const estudiantesNorte = estudiantesOriginal.slice(0,2);
+// DESDE EL 0 HASTA EL 2 SIN INCLUIRLO
+const estudiantesSur=estudiantesOriginal.slice(2);
+console.log(estudiantesOriginal);
+console.log(estudiantesNorte);
+console.log(estudiantesSur);
+estudiantesNorte.pop();
+console.log(estudiantesNorte);
+console.log(estudiantesOriginal);
+estudiantesSur.shift();
+console.log(estudiantesSur);
+
+// UNA BUENA PRACTICA ES DUPLICAR LOS ARRAYS
+
+
+
