806ec7b0d60510cf7291156186ad8d290e682c20
patricioprietogaray <patricioprietogaray@gmail.com>
2022-06-25 00:08:34 -0300

    master

---
+++ clase2.js
@@ -0,0 +1,71 @@
+// Datos compuestos:
+// FUNCIONES DECLARADAS 
+// Son funciones que se declaran y luego son llamadas
+
+function funcionSinArgumentosNoDevuelve() {
+    console.log("No Devuelve solo muestra!");
+}
+
+function funcionConArgumentosNoDevuelve(a, b) {
+    console.log("muestro variable a: "+a);
+    console.log("muestro variable b: "+b);
+}
+
+function funcionSinArgumentosDevuelve() {
+    console.log("Devuelve y solo muestra!");
+    return "retorno de informacion"
+}
+
+function funcionConArgumentosDevuelve(a, b) {
+    console.log("Recibe argumentos y Devuelve!");
+    console.log("muestro variable a: "+a);
+    console.log("muestro variable b: "+b);
+    return "retorno de informacion como suma a + b: "+(a+b);
+}
+
+function funcionConArgumentosDefault(a=10, b=20) {
+    // si no paso argumentos a sera igual a 10
+    // y b será igual a 20
+    return a + b;
+}
+
+console.log("funcion sin argumentos y no devuelve: ");
+funcionSinArgumentosNoDevuelve();
+console.log("*************************");
+
+console.log("funcion con argumentos y no devuelve: ");
+funcionConArgumentosNoDevuelve(10, 20);
+console.log("*************************");
+
+console.log("funcion sin argumentos y devuelve: ");
+let devuelve = funcionSinArgumentosDevuelve();
+console.log("devuelve: "+devuelve);
+console.log("*************************");
+
+console.log("funcion con argumentos y devuelve: ");
+let devuelve2= funcionConArgumentosDevuelve(10, 20);
+console.log("devuelve: "+devuelve2);
+console.log("*************************");
+
+console.log("funcion con argumentos default: ");
+let devuelve3= funcionConArgumentosDefault();
+console.log("devuelve: "+devuelve3);
+console.log("*************************");
+
+// funciones EXPRESADAS
+// Una funcion expresada se le asigna a una variable
+// y no puede invocarse antes de su definicion
+
+let resultado = function() {
+    return 3+4;
+}
+// mostrar la funcion
+console.log(resultado);
+// muestra el resultado
+// resultado lo tomo como el nombre
+// de la funcion
+const res = resultado();
+console.log("3+7="+res);
+
+//////////////
+
--- index.js
+++ index.js
@@ -1,68 +1,29 @@
-// Datos compuestos:
-// FUNCIONES DECLARADAS 
-// Son funciones que se declaran y luego son llamadas
+const result = suma();
+console.log(`El resultado es ${result}`);
 
-function funcionSinArgumentosNoDevuelve() {
-    console.log("No Devuelve solo muestra!");
+function suma(a=1, b=1) {
+    return a+b;
 }
 
-function funcionConArgumentosNoDevuelve(a, b) {
-    console.log("muestro variable a: "+a);
-    console.log("muestro variable b: "+b);
+// ///////// hoistin o algo asi es cuando js permite
+// que la funcion se "mueva hacia arriba" para
+// permitir que se lea la funcion antes de ser
+// llamada, con la funcion expresada no se da,
+// por lo tanto tira error.
+
+// const result01= suma();
+
+// const suma = function () {
+//     return 10;
+// }
+
+// funcion anónima
+// solo funciona asignando una variable o constante
+// function () {
+    // console.log("anonymous");
+// }
+// ej
+function () {
+    console.log("anonymous");
 }
-
-function funcionSinArgumentosDevuelve() {
-    console.log("Devuelve y solo muestra!");
-    return "retorno de informacion"
-}
-
-function funcionConArgumentosDevuelve(a, b) {
-    console.log("Recibe argumentos y Devuelve!");
-    console.log("muestro variable a: "+a);
-    console.log("muestro variable b: "+b);
-    return "retorno de informacion como suma a + b: "+(a+b);
-}
-
-function funcionConArgumentosDefault(a=10, b=20) {
-    // si no paso argumentos a sera igual a 10
-    // y b será igual a 20
-    return a + b;
-}
-
-console.log("funcion sin argumentos y no devuelve: ");
-funcionSinArgumentosNoDevuelve();
-console.log("*************************");
-
-console.log("funcion con argumentos y no devuelve: ");
-funcionConArgumentosNoDevuelve(10, 20);
-console.log("*************************");
-
-console.log("funcion sin argumentos y devuelve: ");
-let devuelve = funcionSinArgumentosDevuelve();
-console.log("devuelve: "+devuelve);
-console.log("*************************");
-
-console.log("funcion con argumentos y devuelve: ");
-let devuelve2= funcionConArgumentosDevuelve(10, 20);
-console.log("devuelve: "+devuelve2);
-console.log("*************************");
-
-console.log("funcion con argumentos default: ");
-let devuelve3= funcionConArgumentosDefault();
-console.log("devuelve: "+devuelve3);
-console.log("*************************");
-
-// funciones EXPRESADAS
-// Una funcion expresada se le asigna a una variable
-// y no puede invocarse antes de su definicion
-
-let resultado = function() {
-    return 3+4;
-}
-// mostrar la funcion
-console.log(resultado);
-// muestra el resultado
-// resultado lo tomo como el nombre
-// de la funcion
-const res = resultado();
-console.log("3+7="+res);
+// 30:00
